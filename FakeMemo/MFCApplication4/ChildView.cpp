5f757cef8d52cb0243d5856f0dd135ecaa8cd581
J <37607965+code243031@users.noreply.github.com>
2021-10-27 16:05:04 +0900

    Add files via upload

---
+++ FakeMemo/MFCApplication4/ChildView.cpp
@@ -0,0 +1,157 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCApplication4.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+	m_color = RGB(0, 0, 0);
+	font.CreatePointFont(100, _T("맑은 고딕"));
+}
+
+CChildView::~CChildView()
+{
+}
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+	ON_WM_CHAR()
+	ON_WM_KILLFOCUS()
+	ON_WM_SETFOCUS()
+	ON_WM_HSCROLL()
+	ON_WM_VSCROLL()
+	ON_COMMAND(ID_FILE_NEW, &CChildView::CreateNewFile)
+	ON_COMMAND(ID_FILE_OPEN, &CChildView::OpenFile)
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+	// 그리기 메시지에 대해서는 CWnd::OnPaint()를 호출하지 마십시오.
+	
+	dc.SelectObject(&font);
+	dc.SetTextColor(m_color);
+
+	CRect rect;
+	GetClientRect(&rect);
+	dc.DrawText(m_str.GetData(), m_str.GetSize(), &rect, DT_LEFT);
+}
+
+void CChildView::OnChar(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+
+	if (nChar == _T('\b')) {
+		if (m_str.GetSize() > 0) m_str.RemoveAt(m_str.GetSize() - 1);
+	}
+	else {
+		m_str.Add(nChar);
+	}
+
+	Invalidate();
+}
+
+
+void CChildView::OnKillFocus(CWnd* pNewWnd)
+{
+	CWnd::OnKillFocus(pNewWnd);
+
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+}
+
+
+void CChildView::OnSetFocus(CWnd* pOldWnd)
+{
+	CWnd::OnSetFocus(pOldWnd);
+
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+}
+
+void CChildView::OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+
+	CWnd::OnHScroll(nSBCode, nPos, pScrollBar);
+}
+
+
+void CChildView::OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+
+	CWnd::OnVScroll(nSBCode, nPos, pScrollBar);
+}
+
+// 메뉴 함수들
+void CChildView::CreateNewFile()
+{
+	// TODO: 여기에 명령 처리기 코드를 추가합니다.
+	m_str.RemoveAll();
+	Invalidate();
+}
+
+void CChildView::OpenFile()
+{
+	// TODO: 여기에 명령 처리기 코드를 추가합니다.
+	CString str = _T("All files(*.*)|*.*|"); // 모든 파일 표시
+	// _T("Excel 파일 (*.xls, *.xlsx) |*.xls; *.xlsx|"); 와 같이 확장자를 제한하여 표시할 수 있음
+	CFileDialog dlg(TRUE, _T("*.dat"), NULL, OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT, str, this);
+
+	if (dlg.DoModal() == IDOK)
+	{
+		CString strPathName = dlg.GetPathName();
+		// 파일 경로를 가져와 사용할 경우, Edit Control에 값 저장
+		CFile file;
+		CFileException e;
+		if (!file.Open(strPathName, CFile::modeReadWrite|CFile::modeCreate|CFile::modeNoTruncate, &e)) {
+			e.ReportError();
+			return;
+		}
+
+		// 작업 처리
+		int len = file.GetLength();
+		char* path = new char[len];
+		file.Read(path, len);
+		file.Close();
+
+		m_str.RemoveAll();
+
+		UINT nChar;
+		for (int i = 0; i < len; i++) {
+			nChar = path[i];
+			m_str.Add(path[i]);
+		}
+
+		Invalidate();
+	}
+
+}
